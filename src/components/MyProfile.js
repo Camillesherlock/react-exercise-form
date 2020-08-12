f1e69f979c72e0c7fc654a7ec09923a76d65823a
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-12 12:26:55 +0800

    [ke] complete basic function

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -4,7 +4,28 @@ import './myProfile.less';
 class MyProfile extends Component {
   render() {
     return (
-      <form></form>
+      <form>
+      <h1>My Profile</h1>
+      <div>
+        <h1>Name</h1>
+        <label htmlFor="name"></label>
+        <input type ="text"/>
+      </div>
+      <section>
+        <h1>Gender</h1>
+       <label for="gender"></label>
+        <select name="bun" class="bun-type" id="bun">
+             <option value="male">male</option>
+            <option value="female">female</option>
+        </select>
+        </section>
+      <div>
+        <h1>discription</h1>
+        <label htmlFor="discription"></label>
+        <input type ="text"/>
+      </div>
+
+      </form>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MyProfile from './components/MyProfile';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<MyProfile />, document.getElementById('root'));
