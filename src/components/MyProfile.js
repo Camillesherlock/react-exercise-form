366243521129d9c735597b98f4dd33f1d7d1320c
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-12 23:32:06 +0800

    [ke] add some function

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,14 +2,33 @@ import React, {Component} from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
+
+  state = {
+    name:"",
+    discription:"",
+  };
+  handleNameChange = (event) =>{
+    this.setState({
+        name: event.target.values,
+    });
+    };
+    handleDescriptionChange = (event) => {
+        this.setState({
+            email:event.target.discription,
+        });
+    };
+    handleSubmit = (event) =>{
+        alert(JSON.stringify(this.state));
+        event.preventDefault();
+    }
   render() {
     return (
-      <form>
+      <form onSubmit = {this.handleSubmit}>
       <h1>My Profile</h1>
       <div>
         <h1>Name</h1>
         <label htmlFor="name"></label>
-        <input type ="text"/>
+        <input type ="text" value = {this.state.name} onChange = {this.handleNameChange}/>
       </div>
       <section>
         <h1>Gender</h1>
@@ -22,9 +41,14 @@ class MyProfile extends Component {
       <div>
         <h1>discription</h1>
         <label htmlFor="discription"></label>
-        <input type ="text"/>
+        <textarea rows="3" cols="50" name="discription" id="discription"></textarea>
       </div>
-
+      <input type="radio" id="yes"/>
+      <label for="yes">I have read the terms of conduct</label>
+      <div>
+      <button>提交</button>
+      </div>
+      
       </form>
     );
   }
